9c97a27c74f56ddbb90679658b4eeb1fd830ae58
Luciana Heuko <lucianaheuko@gmail.com>
2017-01-06 12:27:45 -0200

    contact page

--- src/js/script.js
+++ src/js/script.js
@@ -1,98 +1,43 @@
 // animate header on scroll
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 20){  
-    $('header').addClass("smaller");
-  }
-  else{
-    $('header').removeClass("smaller");
-  }
-});
-
-// animate text on section home
-$(function(){
-  $("#typed").typed({
-    // strings: ["Typed.js is a <strong>jQuery</strong> plugin.", "It <em>types</em> out sentences.", "And then deletes them.", "Try it out!"],
-    strings: [
-      "uma landing page incrível", 
-      "um website atraente", 
-      "uma animação parallax",
-      "um webapp angular", 
-      "uma visualização de dados interativa",
-      "um game",
-      "qualquer coisa que puder imaginar",
-      "tudo o que quiser",
-      "a web"],
-    // stringsElement: $('#typed-strings'),
-    typeSpeed: 60,
-    backDelay: 2000,
-    loop: false,
-    contentType: 'text', // or text
-    // defaults to false for infinite loop
-    loopCount: false,
-    showCursor: true,
-    attr: null,
-    callback: function(){ foo(); },
-    resetCallback: function() { newTyped(); }
+(function () {
+  
+  var $rightCommand = $("#menu-right-command");
+  var $header = $("#header");
+  var isOriginallyLight = $header.hasClass("light");
+    
+  // check if color of header is originally light
+  $(window).scroll(function() {
+    
+    if ($(this).scrollTop() > 20){  
+      $header.addClass("smaller");
+      
+        if (isOriginallyLight) {
+          $header.removeClass("light");
+        }
+    } else {
+      $header.removeClass("smaller");
+      
+      if (isOriginallyLight) {
+        $header.addClass("light");
+      }
+    }
   });
   
-  $(".reset").click(function(){
-      $("#typed").typed('reset');
+  // open and close right-command
+  $rightCommand.on('click', function (e) {
+    
+    var isMenuOpen = $header.hasClass("open-menu");
+    
+    if (isMenuOpen) {
+      $('body').removeClass('scroll-disabled');
+      $header.removeClass('open-menu');
+    } else {
+      $('body').addClass('scroll-disabled');
+      $header.addClass('open-menu');
+    }
+    
+    // e.preventDefault();
+    e.stopPropagation();
   });
-});
-
-
-// animate word change on footer
-$(document).ready(function() {
   
-  var words = [
-    "web",
-    "empoderamento", 
-    "criatividade", 
-    "solução",
-    "poder",
-    "conquista",
-    "transformação",
-    "revolução",
-    "capacidade",
-    "ação",
-    "atitude",
-    "autonomia",
-    "código",
-    "luz",
-    "projeto",
-    "resposta",
-    "liberdade",
-    "igualdade",
-    "democracia",
-    "comunidade",
-    "pavê de chocolate",
-    "pudim",
-    "brigadeiro",
-    "tudo",
-    "café", 
-    "☘",
-    "🤓",
-    "👾",
-    "♫",
-    "😍",
-    "⛄",
-    "🤖",
-    "🚴",
-    "💪🏽",
-    "❤",
-    "🐶",
-    "🐵",
-    "🐽",
-    "🐭",
-    "🍌",
-    "🍄",
-    "🎃",
-    "🎈",
-    "💻",
-  ];
-  $(".words").on("mouseenter", function() {
-    console.log("clicou");
-    var item = words[Math.floor(Math.random()*words.length)];
-    $("#word-change").text(item);
-  });
-});
+})();
