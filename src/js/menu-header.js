6c1b12ad30823cbb4df832243ebf98ddccd433a2
Luciana Heuko <lucianaheuko@gmail.com>
2017-02-07 14:21:47 -0200

    header

--- src/js/menu-header.js
+++ src/js/menu-header.js
@@ -1,33 +1,33 @@
 // animate header on scroll
 $(function () {
-  
+
   var $rightCommand = $("#menu-right-command");
   var $header = $("#header");
   var isOriginallyLight = $header.hasClass("light");
-    
+
   // check if color of header is originally light
   $(window).scroll(function() {
-    
-    if ($(this).scrollTop() > 20){  
+
+    if ($(this).scrollTop() > 20){
       $header.addClass("smaller");
-      
+
         if (isOriginallyLight) {
           $header.removeClass("light");
         }
     } else {
       $header.removeClass("smaller");
-      
+
       if (isOriginallyLight) {
         $header.addClass("light");
       }
     }
   });
-  
+
   // open and close right-command
   $rightCommand.on('click', function (e) {
-    
+
     var isMenuOpen = $header.hasClass("open-menu");
-    
+
     if (isMenuOpen) {
       $('body').removeClass('scroll-disabled');
       $header.removeClass('open-menu');
@@ -35,9 +35,9 @@ $(function () {
       $('body').addClass('scroll-disabled');
       $header.addClass('open-menu');
     }
-    
+
     // e.preventDefault();
     e.stopPropagation();
   });
-  
+
 });
